e2ff4d564d071eb0e174d98bb4cafaa91208a4c7
Laura_Bal <baltrukone@gmail.com>
2024-02-05 17:19:59 +0200

    Mounting the Vue Instance

--- app.js
+++ app.js
@@ -1,36 +1,12 @@
-Vue.createApp ({
+let vm = Vue.createApp ({
 	data () {
 		return {
-			perspective:100,
-			rotateX:0,
-			rotateY:0,
-			rotateZ:0,
-		}
-	},
-	computed:{
-		box () {
-			return {
-				transform:`
-				perspective(${this.perspective}px)
-				rotateX(${this.rotateX}deg)
-				rotateY(${this.rotateY}deg)
-				rotateZ(${this.rotateZ}deg)
-				`
-			}
-		}
-	},
-	methods:{
-		reset(){
-			this.perspective = 100
-			this.rotateX = 0
-			this.rotateY = 0
-			this.rotateZ = 0
-		},
-		async copy(){
-			let text = `transform:${this.box.transform}`;
-			await navigator.clipboard.writeText(text)
-
-			alert("CSS copied to clipboard")
+			message:"Hello world!"
 		}
 	}
-}).mount ('#app');
+})
+vm.mount ('#app')
+
+// setTimeout (() => {
+// 	vm.mount ('#app')
+// }, 3000)
