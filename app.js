cde80bf20b9bc21cde613f0e5780078da8c83ccc
Laura_Bal <baltrukone@gmail.com>
2024-02-12 18:24:15 +0200

    Understanding Reactivity with Proxies

--- app.js
+++ app.js
@@ -1,36 +1,18 @@
 let vm = Vue.createApp ({
 	data () {
-		return {
-			message:"Hello world!"
-		}
-	},
-	beforeCreate(){
-		console.log ('beforeCreate() function called', this.message)
-	},
-	created(){
-		console.log ('created() function called', this.message)
-	},
-	beforeMount(){
-		console.log ('beforeMount() function called', this.$el)
-	},
-	mounted(){
-		console.log ('mounted() function called', this.message)
-	},
-	beforeUpdate(){
-		console.log ('beforeUpdate() function called')
-	},
-	updated(){
-		console.log ('update() function called')
-	},
-	beforeUnmount(){
-		console.log ('beforeUnmount() function called')
-	},
-	unmounted(){
-		console.log ('unmounted() function called')
+		return {}
+	}
+}).mount ('#app');
+
+const data = {
+	name:'Luis'
+}
+const observedData = new Proxy (data, {
+	set (target, key, value) {
+		document.querySelector ("#name").innerHTML = value
+		target[key] = value
 	}
 })
-vm.mount ('#app')
+observedData.name = "John"
+console.log (observedData.name)
 
-// setTimeout (() => {
-// 	vm.mount ('#app')
-// }, 3000)
